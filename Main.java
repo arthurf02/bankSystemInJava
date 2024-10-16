d9c1343dba5a93aad1b7702720a4d0a1fe9eccbe
Arthur Ferreira <arthur.ferreira4163@gmail.com>
2024-10-15 21:27:54 -0300

    Added an Array List for accounts and a function to see account details.

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+        List<ContaCorrente> contas = new ArrayList<>();
         int usuario = 1;
         Scanner newValue = new Scanner(System.in);
-        ContaCorrente cc = new ContaCorrente(0, 0, null, null, 0.0);
         System.out.println("Banco Agiota\nSeu dinheiro, nossa vida!");
         while (usuario != 0){
             System.out.println("MENU\n1-Criar conta\n2-Ver saldo\n3-Ver dados da conta");
@@ -14,10 +16,16 @@ public class Main{
                 usuario = newValue.nextInt();
                 newValue.nextLine();
                 if (usuario == 1){
+                    ContaCorrente cc = new ContaCorrente(1,contas.size()+10000,"","",0.0);
                     cc.criarCC(newValue);
+                    contas.add(cc);
                 }
+            } else if (usuario == 3){
+                System.out.println("Informe o número da conta:");
+                int usuarioConta = newValue.nextInt();
+                newValue.nextLine();
+                contas.get(usuarioConta).verDadosConta();
             }
-            cc.verDadosConta();
         }
         newValue.close();
     }
